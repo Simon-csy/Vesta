f3a3ad229e06bf01a2fda1088e818a52d46d30e1
Simon-csy <ceres.1.sc@gmail.com>
2023-02-17 21:21:23 +0900

    update sample code

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div id="app">
     <a href="https://github.com/luyilin/vue-cute-timeline" class="github-corner"
        aria-label="View source on Github">
@@ -89,4 +89,4 @@ export default {
   }
 
   .github-corner:hover .octo-arm{animation:octocat-wave 560ms ease-in-out}@keyframes octocat-wave{0%,100%{transform:rotate(0)}20%,60%{transform:rotate(-25deg)}40%,80%{transform:rotate(10deg)}}@media (max-width:500px){.github-corner:hover .octo-arm{animation:none}.github-corner .octo-arm{animation:octocat-wave 560ms ease-in-out}}
-</style>
+</style> -->
